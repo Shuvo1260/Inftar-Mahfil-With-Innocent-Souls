58ee7fc0f2efc4894bc63e3712b7db89cc027528
Shuvo <shovocse753@gmail.com>
2020-04-26 06:25:10 +0600

    Project Initialized

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAsn1tsVokdbR9dHZuaLlbSJYnMBwl-ZQA",
+    authDomain: "iftar-mahfil.firebaseapp.com",
+    databaseURL: "https://iftar-mahfil.firebaseio.com",
+    projectId: "iftar-mahfil",
+    storageBucket: "iftar-mahfil.appspot.com",
+    messagingSenderId: "154325578581",
+    appId: "1:154325578581:web:be38097ba1b187dffef5dc",
+    measurementId: "G-46SGQGSFHS",
+    crossDomain: true,
+    crossorigin: true
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
